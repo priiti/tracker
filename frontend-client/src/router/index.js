e850ec4e20de8e2f0d0e9ea62c7f674faa18919e
Priit Pärl <priitparl@gmail.com>
2017-09-08 22:48:18 +0300

    spa component

--- frontend-client/src/router/index.js
+++ frontend-client/src/router/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Hello from '@/components/Hello';
+import Register from '@/components/Register';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'Hello',
-      component: Hello
-    }
-  ]
-})
+      component: Hello,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+    },
+  ],
+});
--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,17 @@
-console.log('Hello')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/register', (req, res) => {
+  res.send({
+    message: `Hello, ${req.body.email}!`,
+  });
+});
+
+app.listen(process.env.APP_PORT || 3000);
